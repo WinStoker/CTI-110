f22222c311544483fdd4230b94e63f34fda433aa
WinStoker <31968363+WinStoker@users.noreply.github.com>
2017-09-14 14:19:39 -0400

    Add files via upload
    Find the distance traveled

---
+++ M2HW1_DistanceTraveled_StokerWinston.py
@@ -0,0 +1,17 @@
+# CTI-110 
+# M2HW1-Distance Traveled  
+# StokerWinston
+# 14 Sept 17
+# Find the distance traveled
+
+#A car traveling at 70mph for 6hrs
+distance = 70*6
+print ('The distance that the car will travel in 6 hours is', distance, 'miles' )
+
+#A car traveling at 70mph for 10hrs
+distance = 70*10 
+print ('The distance that the car will travel in 10 hours is', distance, 'miles' )
+
+#A car traveling at 70mph for 15hrs
+distance = 70*15
+print ('The distance that the car will travel in 15 hours is', distance, 'miles' )
