140ce16eb0d8ff44fb179ec63cb67168b5ce4498
WinStoker <31968363+WinStoker@users.noreply.github.com>
2017-09-14 14:16:52 -0400

    Add files via upload
    Get the projected total sales.

---
+++ M2T1_SalesPrediction_StokerWinston.py
@@ -0,0 +1,14 @@
+#CTI-110
+#M2T1_Sales Prediction
+#StokerWinston
+#7SEPT17
+#Get the projected total sales.
+
+#Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
